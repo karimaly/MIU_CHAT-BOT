c7c32ac39e75f52c0afbd9eac025e1823aa33c90
ahmed ashraf <83847055+AhmedAshraf340@users.noreply.github.com>
2022-02-03 13:22:50 +0200

    mido

--- train_chatbot.py
+++ train_chatbot.py
@@ -19,6 +19,8 @@ import random
 #nltk.download('wordnet')
 
 words=[]
+print("words", words)
+
 classes = []
 documents = []
 ignore_words = ['?', '!']
@@ -49,7 +51,7 @@ words = sorted(list(set(words)))
 # sort classes
 classes = sorted(list(set(classes)))
 # documents = combination between patterns and intents
-print (len(documents), "documents")
+print (len(documents), "documents", documents)
 # classes = intents
 print (len(classes), "classes", classes)
 # words = all words, vocabulary
@@ -67,10 +69,13 @@ output_empty = [0] * len(classes)
 for doc in documents:
     # initialize our bag of words
     bag = []
+    print ("bag is",bag)
     # list of tokenized words for the pattern
     pattern_words = doc[0]
+    print("pattern:", pattern_words)
     # lemmatize each word - create base word, in attempt to represent related words
     pattern_words = [lemmatizer.lemmatize(word.lower()) for word in pattern_words]
+    print("pattern:", pattern_words)
     # create our bag of words array with 1, if word match found in current pattern
     for w in words:
         bag.append(1) if w in pattern_words else bag.append(0)
@@ -85,7 +90,9 @@ random.shuffle(training)
 training = np.array(training, dtype=object)
 # create train and test lists. X - patterns, Y - intents
 train_x = list(training[:,0])
+print ("training",train_x)
 train_y = list(training[:,1])
+print ("training",train_y)
 print("Training data created")
 
 
