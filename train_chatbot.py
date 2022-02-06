0f1547cc3758b282740f353d1aa2b763b495e9f9
ahmed ashraf <83847055+AhmedAshraf340@users.noreply.github.com>
2022-02-06 15:53:42 +0200

    mido

--- train_chatbot.py
+++ train_chatbot.py
@@ -26,7 +26,7 @@ documents = []
 ignore_words = ['?', '!']
 data_file = open('intents.json').read()
 intents = json.loads(data_file)
-#print(data_file)
+print(data_file)
 
 
 #to get all tags from json file (load every thing)
@@ -36,26 +36,33 @@ for intent in intents['intents']:
         #tokenize each word
         w = nltk.word_tokenize(pattern)
         words.extend(w)
+        print("w:",w)
         #add documents in the corpus
         documents.append((w, intent['tag']))
 
         # add to our classes list
         if intent['tag'] not in classes:
             classes.append(intent['tag'])
-            
+        print("classes:", classes)
             
 
 # lemmaztize and lower each word and remove duplicates
 words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in ignore_words]
 words = sorted(list(set(words)))
+print("words", words)
 # sort classes
+print ("classe:",classes)
 classes = sorted(list(set(classes)))
+print ("classes:",classes)
 # documents = combination between patterns and intents
-print (len(documents), "documents", documents)
+print (len(documents))
+print("documents", documents)
 # classes = intents
-print (len(classes), "classes", classes)
+print (len(classes),"classes")
+print("classes", classes)
 # words = all words, vocabulary
-print (len(words), "unique lemmatized words", words)
+print (len(words), "unique lemmatized words")
+print("words",words)
 
 
 pickle.dump(words,open('words.pkl','wb'))
@@ -87,13 +94,14 @@ for doc in documents:
     
     training.append([bag, output_row])
 # shuffle our features and turn into np.array
-random.shuffle(training)
+#random.shuffle(training)
 training = np.array(training, dtype=object)
 # create train and test lists. X - patterns, Y - intents
 train_x = list(training[:,0])
-print ("training",train_x)
+print ("training  x :",train_x)
+
 train_y = list(training[:,1])
-print ("training",train_y)
+print ("training y :",train_y)
 print("Training data created")
 
 
@@ -101,13 +109,15 @@ print("Training data created")
 # equal to number of intents to predict output intent with softmax
 model = Sequential()
 model.layers
-#2 to power and feature extractions
+#2 to power 7 equal 128 and feature extractions
+# 2 to power 6 equas 64
 #try and error
-model.add(Dense( 128, input_shape=(len(train_x[0]),), activation='relu'))
+model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(64, activation='relu'))
 model.add(Dropout(0.5))
 #3adad el output
+#train y = tags el 3andy zero's we one's 3ala hasb eloutput row
 model.add(Dense(len(train_y[0]), activation='softmax'))
 #model summary
 model.summary()
@@ -115,6 +125,7 @@ model.summary()
 sgd = SGD(learning_rate=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 print ("sgd",sgd)
 model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+print("categorical_crossentropy",model.compile)
 #model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 #fitting and saving the model 
